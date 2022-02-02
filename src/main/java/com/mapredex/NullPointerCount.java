17d1e830835db0ca81c076f2272d106269dd1986
D.S. Kosala <koshalads.22@uom.lk>
2022-02-02 14:00:20 +0530

    Initial commit

---
+++ src/main/java/com/mapredex/NullPointerCount.java
@@ -0,0 +1,60 @@
+package com.mapredex;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class NullPointerCount {
+
+  public static class NullPointerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text("NullPointerException");
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      if (value.toString().contains("NullPointerException")) {
+          context.write(word, one);
+      }
+    }
+  }
+
+  public static class NullPointerReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(NullPointerCount.class);
+    job.setMapperClass(NullPointerMapper.class);
+    job.setCombinerClass(NullPointerReducer.class);
+    job.setReducerClass(NullPointerReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
